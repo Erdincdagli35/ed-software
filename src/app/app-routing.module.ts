af3d1d089447c13bce038f82a933f49e9be9597e
Erdincdagli35 <erdinc96dagli35@gmail.com>
2026-01-26 12:43:59 +0300

    All project has been added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from '../app/pages/home/home.component';
+
+import { AboutComponent } from '../app/pages/about/about.component';
+import { ProjectsComponent } from '../app/pages/projects/projects.component';
+import { ContactComponent } from '../app/pages/contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
